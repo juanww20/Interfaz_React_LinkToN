eea6874376d303b2b41b6c8d9797458638db8ea0
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-09-05 13:43:12 -0400

    delete: se elimino el proyecto anterior

--- interfaz_react/src/Administrador/Admin_componentes/DatatableComponente.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DatatableComponente() {
-  return (
-    <div>DatatableComponente</div>
-  )
-}
-
-export default DatatableComponente
--- interfaz_react/src/Administrador/Admin_componentes/LoaderConfig.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LoaderConfig() {
-  return (
-    <div>LoaderConfig</div>
-  )
-}
-
-export default LoaderConfig
